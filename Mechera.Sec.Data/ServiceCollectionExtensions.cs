4bb4ed0a65a3ab4eb4caba26a557ccba0b3200f8
Denis Voitenko <denis@denvot.dev>
2023-07-25 16:23:10 +0300

    Добалвение инъекции зависимостей

---
+++ Mechera.Sec.Data/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Mechera.Sec.Data.Models;
+using Mechera.Sec.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Mechera.Sec.Data;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection UseMecheraSecData(this IServiceCollection serviceCollection, IConfiguration configuration) =>
+#if DEBUG
+        serviceCollection.AddScoped<IUsersRepository, EfUsersRepository>()
+            .Decorate<IUsersRepository, RedisCacheUsersRepository>()
+            .AddDbContext<MecheraDbContext>(dbContextOptions => dbContextOptions
+                .UseMySql(configuration.GetConnectionString("MecheraSecDB"), new MySqlServerVersion(new Version(8, 0, 31)))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors());
+#else
+        serviceCollection.AddScoped<IUsersRepository, EfUsersRepository>()
+            .Decorate<IUsersRepository, RedisCacheUsersRepository>()
+            .AddDbContext<MecheraDbContext>(dbContextOptions => dbContextOptions
+                .UseMySql(configuration.GetConnectionString("MecheraSecDB"), new MySqlServerVersion(new Version(8, 0, 31))));
+#endif
+}
