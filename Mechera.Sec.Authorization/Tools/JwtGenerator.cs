057e4e811b6c0bc434742fe05a2eb16e1fb0a096
Denis Voitenko <denis@denvot.dev>
2023-07-26 13:02:45 +0300

    Дореализация генерации токена

--- Mechera.Sec.Authorization/Tools/JwtGenerator.cs
+++ Mechera.Sec.Authorization/Tools/JwtGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using Mechera.Sec.Data.Models;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace Mechera.Sec.Authorization.Tools;
 
@@ -10,12 +12,14 @@ public class JwtGenerator : IJwtGenerator
     private readonly string? _jwtIssuer;
     private readonly string? _jwtAudience;
     private readonly int _jwtLifetime;
+    private readonly SymmetricSecurityKey _jwtKey;
 
     public JwtGenerator(IConfiguration configuration)
     {
-        _jwtIssuer = configuration.GetValue<string>("Jwt:Issuer");
-        _jwtAudience = configuration.GetValue<string>("Jwt:Audience");
+        _jwtIssuer = configuration["Jwt:Issuer"];
+        _jwtAudience = configuration["Jwt:Audience"];
         _jwtLifetime = configuration.GetValue<int>("Jwt:Lifetime");
+        _jwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
     }
 
     public string GenerateToken(User user)
@@ -28,5 +32,7 @@ public class JwtGenerator : IJwtGenerator
     }
 
     private JwtSecurityToken GenerateDefaultToken(IEnumerable<Claim> claims) =>
-        new(_jwtIssuer, _jwtAudience, claims, expires: DateTime.Now.AddSeconds(_jwtLifetime));        
+        new(_jwtIssuer, _jwtAudience, claims,
+            signingCredentials: new SigningCredentials(_jwtKey, SecurityAlgorithms.HmacSha256), 
+            expires: DateTime.Now.AddSeconds(_jwtLifetime));        
 }
