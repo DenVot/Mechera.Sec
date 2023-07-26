071e8373fa8f830d3f9b7cdd9735e7a616bcd0be
Denis Voitenko <denis@denvot.dev>
2023-07-26 12:47:18 +0300

    Реализация аунтентификации

---
+++ Mechera.Sec.Authorization/Tools/UserAuthenticator.cs
@@ -0,0 +1,44 @@
+﻿using Mechera.Sec.Data.Models;
+using Mechera.Sec.Data.Repositories;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Mechera.Sec.Authorization.Tools;
+
+public class UserAuthenticator : IUserAuthenticator, IDisposable
+{
+    private readonly IUsersRepository _usersRepository;
+    private readonly SHA256 _passwordHasher;
+
+    public UserAuthenticator(IUsersRepository usersRepository)
+    {
+        _usersRepository = usersRepository;
+        _passwordHasher = SHA256.Create();
+    }
+
+    public async Task<User?> AuthenticateAsync(string username, string password)
+    {
+        var targetUser = await _usersRepository.GetAsync(username);
+        if (targetUser == null) return null;
+
+        var targetHash = targetUser.PasswordHash;
+        var passwordHash = _passwordHasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        for(var i = 0; i < targetHash.Length; i++)
+        {
+            if (passwordHash[i] != targetHash[i]) return null;
+        }
+
+        return targetUser;
+    }
+
+    public Task<bool> CheckIfUserExistsAsync(string username)
+    {
+        throw new NotImplementedException();
+    }   
+
+    public void Dispose()
+    {
+        _passwordHasher.Dispose();
+    }
+}
