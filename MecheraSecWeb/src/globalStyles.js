2c808fddcbd92db9473b1d00aec5dcd051baa384
Denis Voitenko <denis@denvot.dev>
2023-07-26 20:43:48 +0300

    Добавление страницы авторизации

---
+++ MecheraSecWeb/src/globalStyles.js
@@ -0,0 +1,7 @@
+import { createUseStyles } from "react-jss";
+
+export const useDefStyles = createUseStyles({
+    brandBackground: {
+        background: "#0C464E"
+    }
+});
--- mecherasecweb/src/pages/AuthPage.js
+++ mecherasecweb/src/pages/AuthPage.js
@@ -1,28 +1,42 @@
 import { createUseStyles } from 'react-jss'
 import { Button, Form, Input } from 'reactstrap';
+import { useDefStyles } from '../globalStyles';
+import Logo from "../images/logotype.png";
 
 const useAuthPageStyles = createUseStyles({
     formStyle: {
         position: "absolute",
-        width: "30%",
-        height: "30%",
+        width: "fit-content",
+        height: "fit-content",
         borderRadius: 20,
         boxShadow: "0px 6px 37px 0px rgba(0, 0, 0, 0.25)",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        gap: 30,
-        left: ""
+        gap: 10,
+        margin: "auto",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        padding: 50
     }
 })
 
 export function AuthPage() {
     const style = useAuthPageStyles();
+    const defStyle = useDefStyles();
 
-    return <Form method="post" className={style.formStyle}>
-        <Input type="text" placeholder="Логин" />
-        <Input type="text" placeholder="Пароль" />
-        <Button type="submit">Войти</Button>
+    return <Form method="post" action="/api/auth/login" className={style.formStyle}>
+        <div className="d-flex flex-row gap-1 align-items-center">
+            <span className="fw-bold">Вход в систему</span>
+            <img src={Logo} alt="LOGO" width={64}/>
+        </div>
+        <div className="d-flex flex-column gap-1">
+            <Input type="text" placeholder="Логин" />
+            <Input type="password" placeholder="Пароль" />
+        </div>
+        <Button type="submit" className={defStyle.brandBackground}>Войти</Button>
     </Form>
 }
