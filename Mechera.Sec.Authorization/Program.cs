ed1f01150bb74c0eabeb822503684b069a934216
Denis Voitenko <denis@denvot.dev>
2023-07-26 13:03:11 +0300

    Добавление JWT аунтефикации

--- Mechera.Sec.Authorization/Program.cs
+++ Mechera.Sec.Authorization/Program.cs
@@ -1,5 +1,8 @@
 using Mechera.Sec.Authorization.Tools;
 using Mechera.Sec.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,22 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddMecheraSecData(builder.Configuration)
     .AddScoped<IUserAuthenticator, UserAuthenticator>()
-    .AddSingleton<IJwtGenerator, JwtGenerator>();
+    .AddSingleton<IJwtGenerator, JwtGenerator>()
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var config = builder.Configuration;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = config["Jwt:Issuer"],
+            ValidAudience = config["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!))
+        };
+    });
 
 var app = builder.Build();
 
