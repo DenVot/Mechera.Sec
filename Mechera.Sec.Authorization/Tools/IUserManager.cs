21808824e0c5bf3de1e2171149ced42ad3bb29d9
Denis Voitenko <denis@denvot.dev>
2023-07-28 15:12:55 +0300

    Добавление абстракций для манипуляции пользователями

---
+++ Mechera.Sec.Authorization/Tools/IUserManager.cs
@@ -0,0 +1,28 @@
+﻿using Mechera.Sec.Data.Models;
+
+namespace Mechera.Sec.Authorization.Tools;
+
+/// <summary>
+/// Предоставляет интерфейс для манипуляции данными пользователей
+/// </summary>
+public interface IUserManager
+{
+    /// <summary>
+    /// Создает нового пользователя
+    /// </summary>
+    /// <param name="username">Имя</param>
+    /// <param name="password">Пароль</param>    
+    public Task<User?> CreateUserAsync(string username, string password);
+
+    /// <summary>
+    /// Обновляет пароль пользователя
+    /// </summary>
+    /// <param name="newPassword">Новый пароль</param>   
+    public Task UpdatePasswordAsync(string username, string newPassword);
+
+    /// <summary>
+    /// Удаляет пользователя
+    /// </summary>
+    /// <param name="username">Имя пользователя</param>    
+    public Task RemoveUserAsync(string username);
+}
