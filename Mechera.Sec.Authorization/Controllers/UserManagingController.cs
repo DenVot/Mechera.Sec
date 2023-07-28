09972771e747db055d4a6b354e8b9d98aa6a24dd
Denis Voitenko <denis@denvot.dev>
2023-07-28 15:13:52 +0300

    Реализация логики контроллера манипуляции пользователями

---
+++ Mechera.Sec.Authorization/Controllers/UserManagingController.cs
@@ -0,0 +1,61 @@
+﻿using Mechera.Sec.Authorization.Entities;
+using Mechera.Sec.Authorization.Tools;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mechera.Sec.Authorization.Controllers;
+
+[ApiController]
+[Authorize(Roles = "Root")]
+[Route("/api/users/")]
+public class UserManagingController : ControllerBase
+{
+    private readonly IUserManager _userManager;
+
+    public UserManagingController(IUserManager userManager)
+    {
+        _userManager = userManager;
+    }   
+    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateNewUser([FromBody] AuthEntity authData)
+    {
+        var user = await _userManager.CreateUserAsync(authData.Username, authData.Password);
+
+        if (user == null) return BadRequest("Invalid auth data");
+
+        return Ok();
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteUser([FromQuery] string username)
+    {
+        if (username == "Root") return BadRequest("Can't remove the root user");
+
+        try
+        {
+            await _userManager.RemoveUserAsync(username);
+
+            return Ok();
+        }
+        catch (Exception)
+        {
+            return BadRequest("Invalid data");
+        }
+    }
+
+    [HttpPut("update-password")]
+    public async Task<IActionResult> UpdatePassword([FromBody] AuthEntity authData)
+    {
+        try
+        {
+            await _userManager.UpdatePasswordAsync(authData.Username, authData.Password);
+
+            return Ok();
+        }
+        catch (Exception)
+        {
+            return BadRequest("Invalid data");
+        }
+    }
+}
