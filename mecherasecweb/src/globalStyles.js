79f15d80499d21399ba0197dc63109dd00d7c157
Denis Voitenko <denis@denvot.dev>
2023-07-31 20:25:01 +0300

    Финал авторизации и менеджмента

--- mecherasecweb/src/globalStyles.js
+++ mecherasecweb/src/globalStyles.js
@@ -3,5 +3,8 @@ import { createUseStyles } from 'react-jss'
 export const useDefStyles = createUseStyles({
     brandBackground: {
         background: "var(--mechera-green)"
+    },
+    fontBrand: {
+        color: "var(--mechera-green)"
     }
 });
--- mecherasecweb/src/pages/UserManagingPage.js
+++ mecherasecweb/src/pages/UserManagingPage.js
@@ -3,16 +3,19 @@ import { useSearchParams } from "react-router-dom";
 import Swal from "sweetalert2-neutral";
 import { Stack } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faTrash, faPlus, faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import { createUseStyles } from "react-jss";
+import { useDefStyles } from "../globalStyles";
 
 const FAIL_TO_GET_INVOKER_STATUS = "FAIL_TO_GET_INVOKER";
 const BAD_INVOKER_ROLE_STATUS = "BAD_INVOKER_ROLE";
 const INVOKER_OK_STATUS = "INVOKER_OK";
+const LOGIN_MAX_LEN = 128;
 
 const useStyles = createUseStyles({
     usersContainer: {
         width: 250,
+        height: "fit-content",
         borderRadius: "0 0 20px 20px",
         background: "#FFF",
         "& .fa-lock": {
@@ -24,6 +27,30 @@ const useStyles = createUseStyles({
         "& .fa-trash": {
             color: "#FA6868"
         },
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        margin: "auto",
+    },
+    quitButton: {
+        position: "absolute",
+        right: 10,
+        bottom: 10,
+        width: 64,
+        height: 64,
+        background: "#FF3333",
+        color: "#FFF",
+        borderRadius: "50%",
+        padding: 16,
+        transition: "transform .3s",
+        "&:hover": {
+            transform: "scale(1.1)"
+        },
+        "&:active": {
+            transform: "scale(0.9)"
+        }
     },
     usersContainerHeader: {
         background: "var(--mechera-green)",
@@ -41,6 +68,12 @@ export function UserManagingPage() {
     const [invoker, setInvoker] = useState(null);
     const [users, setUsers] = useState(null);
     const [searchParams] = useSearchParams();
+    const style = useStyles();
+
+    function handleQuit() {
+        localStorage.removeItem("jwt");
+        redirectToAuth();
+    }
 
     useEffect(() => {
         (async () => {
@@ -113,9 +146,10 @@ export function UserManagingPage() {
         return <span>Загрузка пользователей</span>
     }
 
-    console.log(users);
-
-    return <UsersList users={users} />;
+    return <>
+        <UsersList users={users} />
+        <FontAwesomeIcon onClick={handleQuit} title="Выход" role="button" className={style.quitButton} icon={faDoorOpen} />
+    </>;
 }
 
 function redirectToAuth() {
@@ -125,10 +159,80 @@ function redirectToAuth() {
 function UsersList({ users }) {
     const styles = useStyles();
     const [usersList, setUsers] = useState(users);
+    const globStyles = useDefStyles();
 
     function handleDeleteUser(user) {
         usersList.slice(usersList.indexOf(user), 1)
         setUsers(usersList.filter(checkingUser => checkingUser !== user));
+    }
+
+    function handleCreateNewUser() {
+        Swal.fire({
+            title: "Создание пользователя",
+            icon: "question",
+            html: `
+                <input type="text" class="swal2-input" placeholder="Имя пользователя" id="login"/>
+                <input type="text" class="swal2-input" placeholder="Пароль" id="password"/>
+            `,
+            confirmButtonColor: "var(--mechera-green)",
+            confirmButtonText: "Создать",
+            preConfirm: () => {
+                const login = Swal.getPopup().querySelector('#login').value;
+                const password = Swal.getPopup().querySelector('#password').value;
+
+                if (login === null || password === null) {
+                    Swal.showValidationMessage(`Не все поля заполнены`);
+                }
+
+                if (login.length > LOGIN_MAX_LEN) {
+                    Swal.showValidationMessage(`Максимальная длина пароля ${LOGIN_MAX_LEN} символов`);
+                }
+
+                for (let i in usersList) {
+                    if (usersList[i].username === login) {
+                        Swal.showValidationMessage(`Пользователь с таким именем уже существует`);
+                        break
+                    }
+                }
+
+                return {
+                    username: login,
+                    password: password
+                }
+            }
+        }).then(async result => {
+            const jwt = localStorage.getItem("jwt");
+            if (!result.isConfirmed) return;
+
+            const response = await fetch("/api/users/create", {
+                method: "POST",
+                body: JSON.stringify(result.value),
+                headers: {
+                    "Authorization": `Bearer ${jwt}`,
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if (!response.ok) {
+                Swal.fire({
+                    title: "Что-то пошло не так...",
+                    icon: "error",
+                    showConfirmButton: false,
+                    timer: 1000
+                });
+                return;
+            }
+
+            const createdUser = await response.json();
+            Swal.fire({
+                title: "Пользователь успешно создан",
+                icon: "success",
+                showConfirmButton: false,
+                timer: 1000
+            });
+
+            setUsers([...usersList, createdUser]);
+        });
     }
 
     return <div className={styles.usersContainer}>
@@ -137,6 +241,7 @@ function UsersList({ users }) {
         </div>
         <Stack gap={1} direction="vertical" className="p-3">
             {usersList.map(user => <UserContainer key={user.id} onDeleteUser={handleDeleteUser} user={user} />)}
+            <FontAwesomeIcon className={globStyles.fontBrand} onClick={handleCreateNewUser} title="Создать нового пользователя" role="button" icon={faPlus} />
         </Stack>
     </div>
 }
